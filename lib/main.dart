e7fa73f3b80e2b98b83983bee77c960de82297c8
NadeemMasani <34688479+NadeemMasani@users.noreply.github.com>
2020-12-02 16:28:04 -0800

    First branch commit

--- lib/main.dart
+++ lib/main.dart
@@ -25,7 +25,7 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Chat Application'),
+        title: Text('Chat Application Nadeem'),
       ),
       body: Center(
         child: Text('First Screen'),
